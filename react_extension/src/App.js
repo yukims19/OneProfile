fbe6abbc3da29c0b0b283cdbaf8051ed62ca3675
Youxi Li <yol107@ucsd.edu>
2018-07-07 20:12:40 -0700

    context depending on login status

--- react_extension/src/App.js
+++ react_extension/src/App.js
@@ -7,9 +7,13 @@ import {ApolloClient} from 'apollo-client';
 import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {ApolloProvider, Query} from 'react-apollo';
-
+import OneGraphAuth from 'onegraph-auth';
 
 const TabPane = Tabs.TabPane;
+
+const auth = new OneGraphAuth({
+    appId: 'e3d209d1-0c66-4603-8d9e-ca949f99506d',
+});
 const APP_ID = 'e3d209d1-0c66-4603-8d9e-ca949f99506d';
 
 const client = new ApolloClient({
@@ -47,25 +51,98 @@ const GeneralInfo = () => (
     </Query>
 );
 
-
 class App extends Component {
-  render() {
+    constructor(props){
+        super(props);
+        this.state = {
+            github: false,
+            youtube: false,
+            twitter: false,
+            };
+
+        auth.isLoggedIn('github').then(isLoggedIn => {
+            this.setState({
+                github: isLoggedIn
+            })
+        });
+        auth.isLoggedIn('youtube').then(isLoggedIn => {
+            this.setState({
+                youtube: isLoggedIn
+            })
+        });
+        auth.isLoggedIn('twitter').then(isLoggedIn => {
+            this.setState({
+                twitter: isLoggedIn
+            })
+        });
+    }
+    handleClick(service){
+        try {
+            auth.login(service).then(() => {
+                auth.isLoggedIn(service).then(isLoggedIn => {
+                    if (isLoggedIn) {
+                        console.log('Successfully logged in to ' + service);
+                        this.setState({
+                            [service]: isLoggedIn
+                        });
+                    } else {
+                        console.log('Did not grant auth for service ' + service);
+                        this.setState({
+                            service: isLoggedIn
+                        });
+                    }
+                });
+            });
+        } catch (e) {
+            console.error('Problem logging in', e);
+        }
+    }
+    render() {
+        var github_content = <button className="loginbtn loginbtn-github" onClick={()=>this.handleClick("github")}><i className="fab fa-github"></i> Login with GitHub</button>;
+        var youtube_content = <button className="loginbtn loginbtn-youtube" onClick={()=>this.handleClick("youtube")}><i className="fab fa-youtube"></i> Login with Youtube</button>;
+        var twitter_content = <button className="loginbtn loginbtn-twitter" onClick={()=>this.handleClick("twitter")}><i className="fab fa-twitter"></i> Login with Twitter</button>;
+
+        if(this.state.github){
+            github_content = "content";
+        }else{
+            github_content = <button className="loginbtn loginbtn-github" onClick={()=>this.handleClick("github")}><i className="fab fa-github"></i> Login with GitHub</button>;
+        }
+        if(this.state.youtube){
+            console.log("yes?")
+            youtube_content = "content";
+        }else{
+            youtube_content = <button className="loginbtn loginbtn-youtube" onClick={()=>this.handleClick("youtube")}><i className="fab fa-youtube"></i> Login with Youtube</button>;
+        }
+        if(this.state.twitter){
+            twitter_content = "content";
+        }else{
+            twitter_content = <button className="loginbtn loginbtn-twitter" onClick={()=>this.handleClick("twitter")}><i className="fab fa-twitter"></i> Login with Twitter</button>;
+        }
+
     return (
-      <div className="App">
+            <div className="App">
             <Tabs defaultActiveKey="1">
             <TabPane tab={<span>General</span>} key="1">
+            <div className="tab-content" id="general-content">
             <ApolloProvider client={client}>
             <GeneralInfo />
             </ApolloProvider>
+            </div>
             </TabPane>
             <TabPane tab={<span><Icon type="github" />Github</span>} key="2">
-            Tab 2
+            <div className="tab-content" id="github-content">
+            {github_content}
+            </div>
         </TabPane>
             <TabPane tab={<span><Icon type="youtube" />Youtube</span>} key="3">
-            Tab 3
+            <div className="tab-content" id="youtube-content">
+            {youtube_content}
+            </div>
         </TabPane>
             <TabPane tab={<span><Icon type="twitter" />Twitter</span>} key="4">
-            Tab 4
+            <div className="tab-content" id="twitter-content">
+            {twitter_content}
+            </div>
         </TabPane>
             </Tabs>
        </div>
