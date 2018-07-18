f1947a5c1c7e82ab40f36691839caaf7fd97b500
yukims19 <yol107@ucsd.edu>
2018-07-18 13:04:29 -0700

    Convert project to use prettier (#11)

--- react_extension/public/background.js
+++ react_extension/public/background.js
@@ -1,20 +1,22 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
+  chrome.storage.sync.set({ color: "#3aa757" }, function() {
+    console.log("The color is green.");
+  });
+
+  function renderInfo(info, tab) {
+    console.log("item " + info.linkUrl + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+    chrome.windows.create({
+      url: "index.html/?q=" + info.linkUrl,
+      focused: true,
+      type: "popup"
     });
-
-
-    function renderInfo(info, tab) {
-        console.log("item " + info.linkUrl + " was clicked");
-        console.log("info: " + JSON.stringify(info));
-        console.log("tab: " + JSON.stringify(tab));
-        chrome.windows.create({
-            url : "index.html/?q="+info.linkUrl,
-            focused : true,
-            type : "popup"});
-    }
-    var title = "Search %s via OneProfile";
-    var id = chrome.contextMenus.create({"title": "Search \"%s\" via OneProfile",
-									                       "contexts":["link"],
-                                         "onclick": renderInfo});
+  }
+  var title = "Search %s via OneProfile";
+  var id = chrome.contextMenus.create({
+    title: 'Search "%s" via OneProfile',
+    contexts: ["link"],
+    onclick: renderInfo
+  });
 });
